1bef3396ac8c6e4fd3002e8352acc40218e3c7ae
Pranay Jain <jainpranay20@gmail.com>
2025-01-12 01:40:45 +0530

    API Gateway
    Added api gateway

---
+++ APIGateway/src/test/java/com/pranay/gateway/APIGateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pranay.gateway.APIGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
