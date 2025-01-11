4da2425177e68f8fd9b9588592a97b7b6ca26d68
Pranay Jain <jainpranay20@gmail.com>
2025-01-12 01:13:28 +0530

    Registry Service
    To manage the services instance and registry it works as a server
    
    Registry Service - Server
    other Services - works as client

---
+++ ServiceRegistry/src/test/java/com/pranay/registry/ServiceRegistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pranay.registry.ServiceRegistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
